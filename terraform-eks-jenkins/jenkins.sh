c9dba43e5c644c027b2225a1dee1c63725c4a481
dhan8292 <adhananjai8@gmail.com>
2026-03-14 06:40:35 +0000

    Updated

--- terraform-eks-jenkins/jenkins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras install java-openjdk11 -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins -y
-systemctl enable jenkins
-systemctl start jenkins
